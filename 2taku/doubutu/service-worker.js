680a76c6b200704e4dad7f0362b15e0dcef8c23b
ipadtarou <ipadtaroumk2@gmail.com>
2025-07-25 14:59:04 +0000

    1

---
+++ 2taku/doubutu/service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'quizcopy-cache-v1';
+const urlsToCache = [
+  './s1.html',
+  './manifest.json',
+  './service-worker.js',
+  './audio/seikai.mp3',
+  './audio/matigai.mp3',
+  './images/seikai.gif',
+  './images/1.jpg',
+  './images/2.jpg',
+  './images/3.jpg',
+  './images/4.jpg',
+  './images/5.jpg',
+  './images/6.jpg',
+  './images/7.jpg',
+  './images/8.jpg',
+  './images/9.jpg',
+  './images/10.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
